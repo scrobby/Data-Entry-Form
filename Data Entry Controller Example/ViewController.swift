1d562995b7dfe87768f30a54b1af8aa353772bf9
Carl Goldsmith <carl@Carls-MBP.home>
2015-08-11 16:56:25 +0100

    Initial Commit

---
+++ Data Entry Controller Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Data Entry Controller Example
+//
+//  Created by Carl Goldsmith on 11/08/2015.
+//  Copyright (c) 2015 Carl Goldsmith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
